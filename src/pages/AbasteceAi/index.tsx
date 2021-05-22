9b470aa4deee3d2dc8fd67f1f4caec8a2ff7ec05
Allan Mendes <oallanmendes@gmail.com>
2021-05-21 21:56:49 -0300

    first frontend to abasteceai page

--- src/pages/AbasteceAi/index.tsx
+++ src/pages/AbasteceAi/index.tsx
@@ -1,10 +1,57 @@
 import React from 'react';
-import { View, Title } from './styles';
+import {
+  FlatList,
+  Alert,
+  Text,
+} from 'react-native';
 
-export default function AbasteceAi({navigation}){
+import {
+  ScrollView,
+  Logo,
+  Header,
+  BalanceContainer,
+  BalanceDescription,
+  BalanceNumber,
+  Icon,
+  PumpUpButton,
+  PumpUpButtonText,
+  Story,
+  Title,
+} from './styles';
+
+import MenuButton from '../../components/MenuButton';
+
+export default function Home({navigation}) {  
   return (
-    <View>
-      <Title>Abastece Aí</Title>
-    </View>
-  )
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+    >
+        <Header>
+          <Logo source={require('../../assets/logo.png')}/>
+          <MenuButton
+            icon={'menu'}
+            iconSize={20}
+            notificationNumber={3}
+            onPress={() => {
+              Alert.alert('Mensagem', 'Teste')
+            }}
+          />
+        </Header>
+        <BalanceContainer>
+            <BalanceDescription>Saldo em conta</BalanceDescription>
+            <BalanceNumber>R$ 10,19</BalanceNumber>
+            <BalanceDescription>{`Você já economizou R$ 415,77\nusando o abastece aí para abastecimento`}</BalanceDescription>
+        </BalanceContainer> 
+
+        <PumpUpButton>
+          {/* TODO */}
+          <Icon key={''}/>
+          <PumpUpButtonText>Abastecer</PumpUpButtonText>
+        </PumpUpButton>
+
+        <Story>
+            <Title>{`Veja os postos mais\npróximos`}</Title>
+        </Story>
+    </ScrollView>
+  );
 }
--- src/pages/AbasteceAi/styles.ts
+++ src/pages/AbasteceAi/styles.ts
@@ -1,13 +1,83 @@
 import styled from 'styled-components/native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
-export const View = styled.View`
-  flex: 1;
-  background-color: ${props => props.theme.background};
+interface CardProps {
+  index: number;
+  length: number;
+}
+
+interface ImageProps {
+  index: number;
+  length: number;
+}
+
+export const ScrollView = styled.ScrollView`
+  background-color: #3d9be9;
+`;
+
+export const Header = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0px 25px;
+  margin-top: 25px;
+`;
+
+export const Logo = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 5px;
+`;
+
+export const BalanceContainer = styled.View`
+  margin-left: 25px;
+  margin-top: 100px;
+`;
+export const BalanceDescription = styled.Text`
+  color: #fff;
+  font-size: 12px;
+  font-weight: 600;
+`;
+export const BalanceNumber = styled.Text`
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  margin-top: 10px;
+  margin-bottom: 20px;
+`;
+
+export const PumpUpButton = styled.TouchableOpacity`
+  background-color: ${props => props.theme.secondary};
+  flex-direction: row;
+  padding: 20px;
   justify-content: center;
-  align-items: center;
+  border-radius: 20px;
+  margin-left: 25px;
+  margin-right: 25px;
+  margin-top: 20px;
+`;
+
+export const Icon = styled(MaterialIcons)`
+  color: ${props => props.theme.primary};
+`;
+
+export const PumpUpButtonText = styled.Text`
+  color: ${props => props.theme.primary};
+  font-weight: bold;
+`;
+
+export const Story = styled.View`
+  background-color: ${props => props.theme.background};
+  margin-top: 20px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
 `;
 
 export const Title = styled.Text`
-  font-size: 30px;
+  font-size: 24px;
+  margin-left: 25px;
+  margin-top: 50px;
   color: ${props => props.theme.primary};
-`;
+  font-weight: bold;
+  margin-bottom: 50px;
+`
