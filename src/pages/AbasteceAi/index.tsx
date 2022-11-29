82078011778f26f1ae1be17638d503c078a1c84a
Allan Mendes <oallanmendes@gmail.com>
2022-11-29 18:51:21 -0300

    updated expo sdk from 41 to 45

--- src/pages/AbasteceAi/index.tsx
+++ src/pages/AbasteceAi/index.tsx
@@ -30,67 +30,67 @@ import {
 
 import MenuButton from '../../components/MenuButton';
 
-export default function Home({navigation}) {  
+export default function Home({ navigation }) {
   return (
     <ScrollView
       showsVerticalScrollIndicator={false}
     >
-        <Header>
-          <Logo source={require('../../assets/logo.png')}/>
-          <MenuButton
-            icon={'menu'}
-            iconSize={20}
-            notificationNumber={3}
-            onPress={() => {
-              Alert.alert('Mensagem', 'Teste')
-            }}
-          />
-        </Header>
-        <BalanceContainer>
-            <BalanceDescription>Saldo em conta</BalanceDescription>
-            <BalanceNumber>R$ 10,19</BalanceNumber>
-            <BalanceDescription>{`Você já economizou R$ 415,77\nusando o abastece aí para abastecimento`}</BalanceDescription>
-        </BalanceContainer> 
+      <Header>
+        <Logo source={require('../../assets/logo.png')} />
+        <MenuButton
+          icon={'menu'}
+          iconSize={20}
+          notificationNumber={3}
+          onPress={() => {
+            Alert.alert('Mensagem', 'Teste')
+          }}
+        />
+      </Header>
+      <BalanceContainer>
+        <BalanceDescription>Saldo em conta</BalanceDescription>
+        <BalanceNumber>R$ 10,19</BalanceNumber>
+        <BalanceDescription>{`Você já economizou R$ 415,77\nusando o abastece aí para abastecimento`}</BalanceDescription>
+      </BalanceContainer>
 
-        <PumpUpButton>
-          <Icon name="local-gas-station" size={30}/>
-          <PumpUpButtonText>Abastecer</PumpUpButtonText>
-        </PumpUpButton>
+      <PumpUpButton>
+        <Icon name="local-gas-station" size={30} />
+        <PumpUpButtonText>Abastecer</PumpUpButtonText>
+      </PumpUpButton>
 
-        <LastSupplies>
-            <Title>{`Veja os postos mais\npróximos`}</Title>
-            <Title>Últimos abastecimentos</Title>
+      <LastSupplies>
+        <Title>{`Veja os postos mais\npróximos`}</Title>
+        <Title>Últimos abastecimentos</Title>
 
-            <SupplieCard>
-              <IconPlacer>
-              <IconSupplie name="local-gas-station" size={20}/>
-              </IconPlacer>
-              <View>
-                <SupplieCardCategory>Abastecimento</SupplieCardCategory>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                  <SupplieCardDescription>Flashcar Auto Posto Ltda</SupplieCardDescription>
-                  <SupplieCardPrice>R$ 145,62</SupplieCardPrice>
-                </View>
-                <SupplieCardDate>09/mai às 17h11</SupplieCardDate>
-                <SupplieCardCashback>R$ 5,45 de cashback</SupplieCardCashback>
-              </View>
-            </SupplieCard>
+        <SupplieCard>
+          <IconPlacer>
+            <IconSupplie name="local-gas-station" size={20} />
+          </IconPlacer>
+          <View>
+            <SupplieCardCategory>Abastecimento</SupplieCardCategory>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <SupplieCardDescription>Flashcar Auto Posto Ltda</SupplieCardDescription>
+              <SupplieCardPrice>R$ 145,62</SupplieCardPrice>
+            </View>
+            <SupplieCardDate>09/mai às 17h11</SupplieCardDate>
+            <SupplieCardCashback>R$ 5,45 de cashback</SupplieCardCashback>
+          </View>
+        </SupplieCard>
 
-            <SupplieCard>
-              <IconPlacer>
-              <IconSupplie name="local-gas-station" size={20}/>
-              </IconPlacer>
-              <View>
-                <SupplieCardCategory>Abastecimento</SupplieCardCategory>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                  <SupplieCardDescription>Flashcar Auto Posto Ltda</SupplieCardDescription>
-                  <SupplieCardPrice>R$ 145,62</SupplieCardPrice>
-                </View>
-                <SupplieCardDate>09/mai às 17h11</SupplieCardDate>
-                <SupplieCardCashback>R$ 5,45 de cashback</SupplieCardCashback>
-              </View>
-            </SupplieCard>
-        </LastSupplies>
+        <SupplieCard>
+          <IconPlacer>
+            <IconSupplie name="local-gas-station" size={20} />
+          </IconPlacer>
+          <View>
+            <SupplieCardCategory>Abastecimento</SupplieCardCategory>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <SupplieCardDescription>Flashcar Auto Posto Ltda</SupplieCardDescription>
+              <SupplieCardPrice>R$ 145,62</SupplieCardPrice>
+            </View>
+            <SupplieCardDate>09/mai às 17h11</SupplieCardDate>
+            <SupplieCardCashback>R$ 5,45 de cashback</SupplieCardCashback>
+          </View>
+        </SupplieCard>
+      </LastSupplies>
     </ScrollView>
   );
 }
